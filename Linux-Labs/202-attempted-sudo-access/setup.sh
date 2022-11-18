726fbc9ddff0768effd122c3378aebb6b61a5009
het-tanis <scott.champine@gmail.com>
2022-11-17 21:48:04 -0700

    breaking

--- Linux-Labs/202-attempted-sudo-access/setup.sh
+++ Linux-Labs/202-attempted-sudo-access/setup.sh
@@ -1,5 +1,4 @@
-useradd -m baduser
-echo "1234 \n 1234 \n" | passwd baduser
+useradd -m -p 1234 baduser 
 echo "* * * * * root su - baduser -c 'echo \"1234\" | sudo -S ls /root'" > /var/spool/cron/crontabs/root
 chown root:crontab /var/spool/cron/crontabs/root
 chmod 600 /var/spool/cron/crontabs/root

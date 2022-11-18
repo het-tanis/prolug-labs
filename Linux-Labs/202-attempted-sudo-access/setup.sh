038832a05fc247103c0889f26be2fdf9f4651abf
het-tanis <scott.champine@gmail.com>
2022-11-17 18:58:12 -0700

    fixed permissions

--- Linux-Labs/202-attempted-sudo-access/setup.sh
+++ Linux-Labs/202-attempted-sudo-access/setup.sh
@@ -1,6 +1,7 @@
 useradd -m baduser
 cp /root/sudo_attempts.sh /home/baduser/sudo_attempts.sh
-chmod baduser:baduser /home/baduser/sudo_attempts.sh
+chmod 755 /home/baduser/sudo_attempts.sh
+chown baduser:baduser /home/baduser/sudo_attempts.sh
 
 echo "* * * * * baduser /home/baduser/sudo_attempts.sh" > /var/spool/cron/crontabs/baduser
 chown baduser:crontab /var/spool/cron/crontabs/baduser

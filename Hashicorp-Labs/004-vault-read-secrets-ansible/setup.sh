bbf7a1beae11a918a002b3d62bf0b2674aa9b765
het-tanis <scott.champine@gmail.com>
2023-05-09 07:25:04 -0700

    updated setup.sh

--- Hashicorp-Labs/004-vault-read-secrets-ansible/setup.sh
+++ Hashicorp-Labs/004-vault-read-secrets-ansible/setup.sh
@@ -1,12 +1,9 @@
-sleep 5
 apt update
 apt install gpg
-touch /root/gpgdone
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg >/dev/null
 gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-touch /root/keyssigned
 apt -y install vault 
-apt -y insitall ansible
+apt -y install ansible
 
 touch /tmp/finished

f13d9d1454a4900ac9728da8d44dfc1e12b02774
het-tanis <scott.champine@gmail.com>
2023-09-10 06:36:02 -0700

    updated lab startup

--- Kubernetes-Labs/three-tier-app-in-kubernetes/setup.sh
+++ Kubernetes-Labs/three-tier-app-in-kubernetes/setup.sh
@@ -1,7 +1,5 @@
 kubectl create -f /root/setup/ingress_create.yaml
-
 echo "172.30.1.2 application.lab.mine"  >> /etc/hosts
-
 docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
 touch /tmp/finished

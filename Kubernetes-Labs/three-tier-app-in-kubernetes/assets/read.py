e0b57951f7162e0e3a9207c1d26563962f6394bd
het-tanis <scott.champine@gmail.com>
2023-09-10 19:00:49 -0700

    updating the end of all files for upload

--- Kubernetes-Labs/three-tier-app-in-kubernetes/assets/read.py
+++ Kubernetes-Labs/three-tier-app-in-kubernetes/assets/read.py
@@ -18,3 +18,4 @@ def someName():
 if __name__ == '__main__':
           port = int(os.environ.get('PORT', 6000))
           app.run(debug=True, host='0.0.0.0', port=port)
+          
--- Kubernetes-Labs/three-tier-app-in-kubernetes/assets/view.py
+++ Kubernetes-Labs/three-tier-app-in-kubernetes/assets/view.py
@@ -12,3 +12,4 @@ def home():
 if __name__ == "__main__":
 	port = int(os.environ.get('PORT', 6000))
 	app.run(debug=True, host='0.0.0.0', port=port)
+	
